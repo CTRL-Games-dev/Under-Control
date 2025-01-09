33442ae47256bc83ecbe9bec0f05b16e931f5cec
londek <48859717+londek@users.noreply.github.com>
2025-01-09 19:29:04 +0100

    chore: make description a textarea

--- Assets/!/Scripts/Inventory/Item.cs
+++ Assets/!/Scripts/Inventory/Item.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class Item : ScriptableObject
 {
     public string DisplayName;
+    [TextArea]
     public string Description;
     public Sprite Icon;
     public Vector2Int Size;
