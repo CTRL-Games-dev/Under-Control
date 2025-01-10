3ce59bb8eb72701c4b1767e7193de9a6f0ea111f
mikkk <108144274+JustMikkk@users.noreply.github.com>
2025-01-11 00:47:38 +0100

    feat: inventory-ui
    fix: prefab not working
    
    feat: inventory prefab
    
    refactor: zmiana do jednego pliku
    
    feat: auto generating grid and items

---
+++ Assets/!/Scripts/UI/Inventory/InvTile.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InvTile : MonoBehaviour
+{
+    private GameObject _highlightImage;
+
+    private Image _image;
+    public InventoryUIManager InventoryUIManager;
+    
+    private bool _isEmpty = true;
+    public bool IsEmpty {
+        get { return _isEmpty; }
+        set {
+            _isEmpty = value;
+            if (_isEmpty) {
+                _image.color = Color.white;
+            } else {
+                _image.color = Color.gray;
+            }
+        }
+    }
+
+    public Vector2Int Pos { get; set; }
+
+
+    void Awake() {
+        _image = GetComponent<Image>();
+        _highlightImage = transform.GetChild(0).gameObject;
+    }
+
+    public void OnPointerEnter() {
+        InventoryUIManager.SelectedTile = this;
+        if (InventoryUIManager.SelectedInventoryItem != null) {
+            InventoryUIManager.HighlightNeighbours(Pos, InventoryUIManager.SelectedInventoryItem.Size);
+        }
+        
+        _highlightImage.SetActive(true);
+    }
+    public void OnPointerExit() {
+        InventoryUIManager.SelectedTile = null;
+        InventoryUIManager.ClearHighlights();
+    }
+    public void OnPointerClick() {
+        InventoryUIManager.TryMoveSelectedItem();
+    }
+    public void OnDrop() {
+        InventoryUIManager.TryMoveSelectedItem();
+    }
+
+    public void SetHighlight(bool value) {
+        // if (!IsEmpty) {
+        //     _highlightImage.GetComponent<Image>().color = Color.red;
+        // } else {
+        //     _highlightImage.GetComponent<Image>().color = Color.green;
+        // }
+        _highlightImage.SetActive(value);
+    }
+}
