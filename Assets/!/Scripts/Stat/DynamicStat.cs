64144b597bcc9de56ecf3a1c8688c863628823ce
londek <48859717+londek@users.noreply.github.com>
2025-01-21 21:22:49 +0100

    fix: correctly show stats in editor (woohoo)

--- Assets/!/Scripts/Stat/DynamicStat.cs
+++ Assets/!/Scripts/Stat/DynamicStat.cs
@@ -53,22 +53,69 @@ public class DynamicStat : Stat {
 }
 
 #if UNITY_EDITOR
+// ile ja nad tym pracowalem wtf
 [CustomPropertyDrawer(typeof(DynamicStat))]
 public class DynamicStatPropertyDrawer : PropertyDrawer {
-    public override VisualElement CreatePropertyGUI(SerializedProperty property) {
-        var propertiesContainer = new Foldout { text = property.displayName, value = true };
+    public override VisualElement CreatePropertyGUI(SerializedProperty property)
+    {
+        // Hack to get the ACTUAL label width
+        bool hierarchyMode = EditorGUIUtility.hierarchyMode;
+        EditorGUIUtility.hierarchyMode = true;
+        float labelWidth = EditorGUIUtility.labelWidth;
+        EditorGUIUtility.hierarchyMode = hierarchyMode;
 
-        var statTypePropertyField = new PropertyField(property.FindPropertyRelative("statType"));
-        propertiesContainer.Add(statTypePropertyField);
+        var container = new VisualElement();
+        container.AddToClassList("unity-base-field");
+        container.AddToClassList("unity-base-composite-field");
+        container.AddToClassList("unity-base-field__aligned");
+        container.AddToClassList("unity-base-field__inspector-field");
 
-        var rawPropertyField = new PropertyField(property.FindPropertyRelative("_raw"));
-        propertiesContainer.Add(rawPropertyField);
+        var label = new Label(property.displayName);
+        label.AddToClassList("unity-base-field__label");
+        label.AddToClassList("unity-base-text-field__label");
+        label.AddToClassList("unity-float-field__label");
+        label.AddToClassList("unity-base-field__label--with-dragger");
+        label.AddToClassList("unity-property-field__label");
 
-        var adjustedPropertyField = new PropertyField(property.FindPropertyRelative("_adjusted"));
-        adjustedPropertyField.SetEnabled(false);
-        propertiesContainer.Add(adjustedPropertyField);
+        label.style.width = labelWidth;
 
-        return propertiesContainer;
+        var inputContainer = new VisualElement();
+        inputContainer.style.flexDirection = FlexDirection.Row;
+
+        inputContainer.AddToClassList("unity-base-field__input");
+        inputContainer.AddToClassList("unity-property-field__input");
+        inputContainer.AddToClassList("unity-composite-field__input");
+        inputContainer.AddToClassList("unity-vector2-field__input");
+        
+        // // Create property fields
+        var rawField = new FloatField();
+        rawField.style.flexGrow = 1;
+        rawField.style.flexBasis = 0;
+        rawField.style.marginLeft = 0;
+        rawField.BindProperty(property.FindPropertyRelative("_raw"));
+        
+        var arrowLabel = new Label("=>");
+        arrowLabel.style.width = 30;
+        arrowLabel.style.flexGrow = 0;
+        arrowLabel.style.paddingLeft = 0;
+        arrowLabel.style.paddingRight = 0;
+        arrowLabel.style.unityTextAlign = TextAnchor.MiddleCenter;
+        
+        var adjustedField = new FloatField();
+        adjustedField.style.flexBasis = 0;
+        adjustedField.style.flexGrow = 1;
+        adjustedField.style.marginLeft = 0;
+        adjustedField.BindProperty(property.FindPropertyRelative("_adjusted"));
+        adjustedField.SetEnabled(false);
+        
+        inputContainer.Add(rawField);
+        inputContainer.Add(arrowLabel);
+        inputContainer.Add(adjustedField);
+
+        container.Add(label);
+        container.Add(inputContainer);
+        
+        return container;
     }
 }
 #endif
--- Assets/!/Scripts/Stat/Stat.cs
+++ Assets/!/Scripts/Stat/Stat.cs
@@ -36,13 +36,69 @@ public class Stat {
 }
 
 #if UNITY_EDITOR
+// ile ja nad tym pracowalem wtf
 [CustomPropertyDrawer(typeof(Stat))]
 public class StatPropertyDrawer : PropertyDrawer {
-    public override VisualElement CreatePropertyGUI(SerializedProperty property) {
-        var adjustedPropertyField = new PropertyField(property.FindPropertyRelative("_adjusted"), property.displayName);
-        adjustedPropertyField.SetEnabled(false);
+    public override VisualElement CreatePropertyGUI(SerializedProperty property)
+    {
+        // Hack to get the ACTUAL label width
+        bool hierarchyMode = EditorGUIUtility.hierarchyMode;
+        EditorGUIUtility.hierarchyMode = true;
+        float labelWidth = EditorGUIUtility.labelWidth;
+        EditorGUIUtility.hierarchyMode = hierarchyMode;
 
-        return adjustedPropertyField;
+        var container = new VisualElement();
+        container.AddToClassList("unity-base-field");
+        container.AddToClassList("unity-base-composite-field");
+        container.AddToClassList("unity-base-field__aligned");
+        container.AddToClassList("unity-base-field__inspector-field");
+
+        var label = new Label(property.displayName);
+        label.AddToClassList("unity-base-field__label");
+        label.AddToClassList("unity-base-text-field__label");
+        label.AddToClassList("unity-float-field__label");
+        label.AddToClassList("unity-base-field__label--with-dragger");
+        label.AddToClassList("unity-property-field__label");
+
+        label.style.width = labelWidth;
+
+        var inputContainer = new VisualElement();
+        inputContainer.style.flexDirection = FlexDirection.Row;
+
+        inputContainer.AddToClassList("unity-base-field__input");
+        inputContainer.AddToClassList("unity-property-field__input");
+        inputContainer.AddToClassList("unity-composite-field__input");
+        inputContainer.AddToClassList("unity-vector2-field__input");
+        
+        // // Create property fields
+        var rawField = new FloatField();
+        rawField.style.flexGrow = 1;
+        rawField.style.flexBasis = 0;
+        rawField.style.marginLeft = 0;
+        rawField.BindProperty(property.FindPropertyRelative("_raw"));
+        
+        var arrowLabel = new Label("=>");
+        arrowLabel.style.width = 30;
+        arrowLabel.style.flexGrow = 0;
+        arrowLabel.style.paddingLeft = 0;
+        arrowLabel.style.paddingRight = 0;
+        arrowLabel.style.unityTextAlign = TextAnchor.MiddleCenter;
+        
+        var adjustedField = new FloatField();
+        adjustedField.style.flexBasis = 0;
+        adjustedField.style.flexGrow = 1;
+        adjustedField.style.marginLeft = 0;
+        adjustedField.BindProperty(property.FindPropertyRelative("_adjusted"));
+        adjustedField.SetEnabled(false);
+        
+        inputContainer.Add(rawField);
+        inputContainer.Add(arrowLabel);
+        inputContainer.Add(adjustedField);
+
+        container.Add(label);
+        container.Add(inputContainer);
+        
+        return container;
     }
 }
 #endif
