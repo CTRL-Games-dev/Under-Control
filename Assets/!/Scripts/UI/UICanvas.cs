f95d0d6a1eda806ff2331c64c8dd350dab718d48
londek <48859717+londek@users.noreply.github.com>
2025-01-22 14:26:16 +0100

    fix: change way uicanvas gets references

--- Assets/!/Scripts/UI/UICanvas.cs
+++ Assets/!/Scripts/UI/UICanvas.cs
@@ -1,10 +1,32 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class UICanvas : MonoBehaviour
 {
     [Header("References for children")]
     public GameObject Player;
-    public PlayerController PlayerController;
-    public LivingEntity PlayerLivingEntity;
-    public EntityInventory PlayerInventory;
+
+    private PlayerController _playerController;
+    public PlayerController PlayerController {
+        get {
+            if (_playerController == null) {
+                _playerController = Player.GetComponent<PlayerController>();
+            }
+            
+            return _playerController;
+        }
+    }
+
+    private LivingEntity _livingEntity;
+    public LivingEntity PlayerLivingEntity {
+        get {
+            if (_livingEntity == null) {
+                _livingEntity = Player.GetComponent<LivingEntity>();
+            }
+
+            return _livingEntity;
+        }
+    }
+
+    public EntityInventory PlayerInventory { get => PlayerLivingEntity.Inventory; }
 }
