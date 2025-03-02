b481096ce271920cf0c59a152e20027fddc432bd
mikkk <108144274+JustMikkk@users.noreply.github.com>
2025-03-02 19:08:42 +0100

    feat: chest

--- Assets/!/Scripts/Objects/Chest.cs
+++ Assets/!/Scripts/Objects/Chest.cs
@@ -5,13 +5,15 @@ public class Chest : MonoBehaviour, IInteractable
 {
     [SerializeField] private GameObject _uiPrefab;
     private SimpleInventory _inventory;
+    private Animator _animator;
 
     void Awake() {
         _inventory = GetComponent<SimpleInventory>();
+        _animator = GetComponent<Animator>();
     }
 
-    public void Interact(PlayerController player)
-    {
+    public void Interact(PlayerController player) {
+        _animator.SetTrigger("open");
         UICanvas.Instance.SetOtherInventory(_inventory.ItemContainer, _uiPrefab);
     }
 }
