146d74bdd6d7e3f19648d5e5f9773ad1836828ef
londek <48859717+londek@users.noreply.github.com>
2025-03-06 21:47:44 +0100

    fix: merge issues

--- Assets/!/Scripts/ItemEntity/ItemEntity.cs
+++ Assets/!/Scripts/ItemEntity/ItemEntity.cs
@@ -36,34 +36,6 @@ public class ItemEntity : MonoBehaviour, IInteractable
 
     void Awake() {
         Rigidbody = GetComponent<Rigidbody>();
-    public Rigidbody Rigidbody { get; private set; }
-
-    public static ItemEntity Spawn(ItemData itemData, int amount, Vector3 position, Quaternion quaternion) {
-        ItemEntity itemEntity = Instantiate(GameManager.Instance.ItemEntityPrefab, position, quaternion);
-        itemEntity.Amount = amount;
-        itemEntity.ItemData = itemData;
-
-        if(itemData.Model != null) {
-            Instantiate(itemData.Model, itemEntity.transform);
-        } else {
-            Instantiate(GameManager.Instance.UnknownModelPrefab, itemEntity.transform);
-        }
-
-        return itemEntity;
-    }
-
-    public static ItemEntity Spawn(ItemData itemData, int amount, Vector3 position) {
-        return Spawn(itemData, amount, position, Quaternion.identity);
-    }
-
-    public static ItemEntity SpawnThrownRelative(ItemData itemData, int amount, Vector3 position, Quaternion quaternion, Vector3 force) {
-        ItemEntity itemEntity = Spawn(itemData, amount, position, quaternion);
-        itemEntity.Rigidbody.AddRelativeForce(force, ForceMode.Impulse);
-        return itemEntity;
-    }
-
-    void Awake() {
-        Rigidbody = GetComponent<Rigidbody>();
     }
 
     public void Interact(PlayerController player) {
