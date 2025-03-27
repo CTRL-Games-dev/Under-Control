2cf5e57e778fa50a70eae061d3ee768092af5a4c
mikkk <108144274+JustMikkk@users.noreply.github.com>
2025-03-27 07:50:41 +0100

    feat: choose run modifier canvas (#59)
    to check it out change uicanvas middle ui state to 'Chose'  and disable hubmanager

--- Assets/!/Scripts/EventBus.cs
+++ Assets/!/Scripts/EventBus.cs
@@ -5,9 +5,8 @@ public static class EventBus {
     public static UnityEvent ItemPlacedEvent = new();
     public static UnityEvent<ItemUI> ItemUILeftClickEvent = new();
     public static UnityEvent<ItemUI> ItemUIRightClickEvent = new();
-    public static UnityEvent<InvTile> InvTileClickEvent = new();
     public static UnityEvent<ItemUI> ItemUIHoverEvent = new(); 
     public static UnityEvent TileSizeSetEvent = new();
-    public static UnityEvent InventoryOpenEvent = new();
-    public static UnityEvent InventoryCloseEvent = new();
+
+    public static UnityEvent<RunModifier> RunCardClickedEvent = new();
 }
--- Assets/!/Scripts/Player/RunStats.cs
+++ Assets/!/Scripts/Player/RunStats.cs
@@ -37,37 +37,105 @@ public class RunStats : MonoBehaviour
     public float DashCooldown = 5;
     public float DashDistance = 5;
 
-    private Dictionary<RunStat, float> _runStats;
 
-    private void Awake() {
-        _runStats = new Dictionary<RunStat, float>{
-            {RunStat.MaxHealth, MaxHealth},
-            {RunStat.Health, Health},
-            {RunStat.MaxMana, MaxMana},
-            {RunStat.Mana, Mana},
-            {RunStat.Armor, Armor},
-            {RunStat.LightAttackDamage, LightAttackDamage},
-            {RunStat.LightAttackSpeed, LightAttackSpeed},
-            {RunStat.LightAttackrange, LightAttackRange},
-            {RunStat.HeavyAttackDamage, HeavyAttackDamage},
-            {RunStat.HeavyAttackSpeed, HeavyAttackSpeed},
-            {RunStat.HeavyAttackRange, HeavyAttackRange},
-            {RunStat.MovementSpeed, MovementSpeed},
-            {RunStat.DashSpeed, DashSpeed},
-            {RunStat.DashCooldown, DashCooldown},
-            {RunStat.DashDistance, DashDistance},
-        };
-    }
 
     public void SetStat(RunStat stat, float value) {
-        _runStats[stat] = value;
+        setStatValue(stat, value);
     }
 
     public void AddToStat(RunStat stat, float value) {
-        _runStats[stat] += value;
+        setStatValue(stat, getStatValue(stat) + value);
     }
 
-    public float GetStat(RunStat stat) {
-        return _runStats[stat];
+    public void AddRunModifier(RunModifier runModifier) {
+        AddToStat(runModifier.Modifier, runModifier.ModifierValue);
+    }
+
+    private void setStatValue(RunStat stat, float value) {
+        switch(stat) {
+            case RunStat.MaxHealth:
+                MaxHealth = value;
+                return;
+            case RunStat.Health:
+                Health = value;
+                return;
+            case RunStat.MaxMana:
+                MaxMana = value;
+                return;
+            case RunStat.Mana:
+                Mana = value;
+                return;
+            case RunStat.Armor:
+                Armor = value;
+                return;
+            case RunStat.LightAttackDamage:
+                LightAttackDamage = value;
+                return;
+            case RunStat.LightAttackSpeed:
+                LightAttackSpeed = value;
+                return;
+            case RunStat.LightAttackrange:
+                LightAttackRange = value;
+                return;
+            case RunStat.HeavyAttackDamage:
+                HeavyAttackDamage = value;
+                return;
+            case RunStat.HeavyAttackSpeed:
+                HeavyAttackSpeed = value;
+                return;
+            case RunStat.HeavyAttackRange:
+                HeavyAttackRange = value;
+                return;
+            case RunStat.MovementSpeed:
+                MovementSpeed = value;
+                return;
+            case RunStat.DashSpeed:
+                DashSpeed = value;
+                return;
+            case RunStat.DashCooldown:
+                DashCooldown = value;
+                return;
+            case RunStat.DashDistance:
+                DashDistance = value;
+                return;
+        }
+    }
+
+
+    private float getStatValue(RunStat stat) {
+        switch(stat) {
+            case RunStat.MaxHealth:
+                return MaxHealth;
+            case RunStat.Health:
+                return Health;
+            case RunStat.MaxMana:
+                return MaxMana;
+            case RunStat.Mana:
+                return Mana;
+            case RunStat.Armor:
+                return Armor;
+            case RunStat.LightAttackDamage:
+                return LightAttackDamage;
+            case RunStat.LightAttackSpeed:
+                return LightAttackSpeed;
+            case RunStat.LightAttackrange:
+                return LightAttackRange;
+            case RunStat.HeavyAttackDamage:
+                return HeavyAttackDamage;
+            case RunStat.HeavyAttackSpeed:
+                return HeavyAttackSpeed;
+            case RunStat.HeavyAttackRange:
+                return HeavyAttackRange;
+            case RunStat.MovementSpeed:
+                return MovementSpeed;
+            case RunStat.DashSpeed:
+                return DashSpeed;
+            case RunStat.DashCooldown:
+                return DashCooldown;
+            case RunStat.DashDistance:
+                return DashDistance;
+            default:
+                return 0;
+        }
     }
 }
