0571730256fc4efa81d550f2f13d2b585103228c
londek <48859717+londek@users.noreply.github.com>
2025-03-28 13:22:38 +0100

    fix: modify rich text modifier format

--- Assets/!/Scripts/Modifier/Modifier.cs
+++ Assets/!/Scripts/Modifier/Modifier.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 
 [Serializable]
 public struct Modifier {
+    public const string POSITIVE_COLOR = "#00FF00";
+    public const string NEUTRAL_COLOR = "#FFFFFF";
+    public const string NEGATIVE_COLOR = "#FF0000";
+
     public StatType StatType;
     public float Value;
     public ModifierType Type;
@@ -57,24 +61,49 @@ public struct Modifier {
     }
 
     public string ToRichTextString() {
-        ModifierImpact impact = GetImpact();
+        string valueString;
+        switch(Type) {
+            case ModifierType.ADDITIVE:
+                if (Value > 0) {
+                    valueString = $"+{Value}";
+                } else {
+                    valueString = $"{Value}";
+                }
 
-        string color;
-        switch(impact) {
-            case ModifierImpact.POSITIVE:
-                color = "#00FF00";
                 break;
-            case ModifierImpact.NEUTRAL:
-                color = "#FFFFFF";
+            case ModifierType.MULTIPLICATIVE:
+                valueString = $"{Value}x";
                 break;
-            case ModifierImpact.NEGATIVE:
-                color = "#FF0000";
+            case ModifierType.PERCENT_CHANGE:
+                if (Value > 0) {
+                    valueString = $"+{Value}%";
+                } else {
+                    valueString = $"{Value}%";
+                }
+
                 break;
             default:
                 throw new ArgumentOutOfRangeException();
         }
 
-        return $"<color={color}>{ToString()}</color>";
+        ModifierImpact impact = GetImpact();
+
+        string color;
+        switch(impact) {
+            case ModifierImpact.POSITIVE:
+                color = POSITIVE_COLOR;
+                break;
+            case ModifierImpact.NEUTRAL:
+                color = NEUTRAL_COLOR;
+                break;
+            case ModifierImpact.NEGATIVE:
+                color = NEGATIVE_COLOR;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+
+        return $"<color={color}>{valueString}</color> {StatType.GetDisplayName()}";
     }
 
     public ModifierImpact GetImpact() {
