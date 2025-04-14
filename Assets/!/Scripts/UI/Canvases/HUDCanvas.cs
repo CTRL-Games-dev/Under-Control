a4d0d42eb10647b53670a8759b60a9a258cbf88c
mikkk <108144274+JustMikkk@users.noreply.github.com>
2025-04-14 23:12:46 +0200

    feat: consumables on hud

--- Assets/!/Scripts/UI/Canvases/HUDCanvas.cs
+++ Assets/!/Scripts/UI/Canvases/HUDCanvas.cs
@@ -31,6 +31,22 @@ public class HUDCanvas : MonoBehaviour, IUICanvasState
     [SerializeField] private TextMeshProUGUI _consumable1Text;
     [SerializeField] private TextMeshProUGUI _consumable2Text;
 
+    [SerializeField] private RectTransform _consumable1Rect;
+    [SerializeField] private RectTransform _consumable2Rect;
+
+    [SerializeField] private Image _consumable1CooldownImg;
+    [SerializeField] private Image _consumable2CooldownImg;
+
+    [Header("Colors")]
+    [SerializeField] private Color _defaultColor;
+    [SerializeField] private Color _manaColor;
+    [SerializeField] private Color _healthColor;
+
+
+    private void Start() {
+        Player.Instance.UpdateConsumablesEvent.AddListener(OnUpdateConsumables);
+    }
+
 
     private void Update() {
         UpdateHealthBar();
@@ -47,6 +63,95 @@ public class HUDCanvas : MonoBehaviour, IUICanvasState
         _manaText.text = $"{(int)Player.Instance.Mana}/{(int)Player.Instance.MaxMana}";
     }
 
+
+    public void OnUpdateConsumables() {
+        if (Player.Instance.ConsumableItemOne != null) {
+            if (Player.Instance.ConsumableItemOne.ItemData == null) {
+                _consumable1Img.gameObject.SetActive(false);
+                _consumable1CooldownImg.gameObject.SetActive(false);
+                return;
+            }
+            _consumable1Img.gameObject.SetActive(true);
+            _consumable1CooldownImg.gameObject.SetActive(true);
+            _consumable1Img.sprite = Player.Instance.ConsumableItemOne.ItemData.Icon;
+            _consumable1Text.text = Player.Instance.ConsumableItemOne.Amount.ToString();
+
+            if (Player.Instance.ConsumableItemOne.ItemData is HealthPotionItemData) {
+                _consumable1CooldownImg.color = _healthColor;
+            } else if (Player.Instance.ConsumableItemOne.ItemData is ManaPotionItemData) {
+                _consumable1CooldownImg.color = _manaColor;
+            } else {
+                _consumable1CooldownImg.color = _defaultColor;
+            }
+
+        } else {
+            _consumable1Img.sprite = null;
+            _consumable1Text.text = "";
+            _consumable1Img.gameObject.SetActive(false);
+            _consumable1CooldownImg.gameObject.SetActive(false);
+        }
+
+        if (Player.Instance.ConsumableItemTwo != null) {
+            if (Player.Instance.ConsumableItemTwo.ItemData == null) {
+                _consumable2Img.gameObject.SetActive(false);
+                _consumable2CooldownImg.gameObject.SetActive(false);
+                return;
+            }
+            _consumable2Img.gameObject.SetActive(true);
+            _consumable2CooldownImg.gameObject.SetActive(true);
+            _consumable2Img.sprite = Player.Instance.ConsumableItemTwo.ItemData.Icon;
+            _consumable2Text.text = Player.Instance.ConsumableItemTwo.Amount.ToString();
+
+            if (Player.Instance.ConsumableItemTwo.ItemData is HealthPotionItemData) {
+                _consumable2CooldownImg.color = _healthColor;
+            } else if (Player.Instance.ConsumableItemTwo.ItemData is ManaPotionItemData) {
+                _consumable2CooldownImg.color = _manaColor;
+            } else {
+                _consumable2CooldownImg.color = _defaultColor;
+            }
+
+        } else {
+            _consumable2Img.sprite = null;
+            _consumable2Text.text = "";
+            _consumable2Img.gameObject.SetActive(false);
+            _consumable2CooldownImg.gameObject.SetActive(false);
+        }
+    }
+
+    public void UseConsumable1() {
+        useConsumable(1);
+    }
+
+    public void UseConsumable2() {
+        useConsumable(2);
+    }
+
+    private void useConsumable(int type) {
+        RectTransform consumableRect = type == 1 ? _consumable1Rect : _consumable2Rect;
+
+        consumableRect.DOComplete();
+        consumableRect.DOKill();
+
+        consumableRect.DOShakeRotation(0.1f * Settings.AnimationSpeed, 10, 10, 90, false);
+        consumableRect.DOScale(Vector3.one * 0.95f, 0.1f * Settings.AnimationSpeed).SetEase(Ease.OutQuint).OnComplete(() => {
+            consumableRect.DOScale(Vector3.one, 0.1f * Settings.AnimationSpeed).SetEase(Ease.OutQuint);
+        });
+        
+        _consumable1CooldownImg.DOFillAmount(1, 0.05f * Settings.AnimationSpeed).SetEase(Ease.OutQuint).OnComplete(() => {
+            _consumable1CooldownImg.DOFillAmount(0, Player.Instance.ConsumableCooldown.CooldownTime - 0.05f * Settings.AnimationSpeed).SetEase(Ease.Linear);
+        });
+        _consumable2CooldownImg.DOFillAmount(1, 0.05f * Settings.AnimationSpeed).SetEase(Ease.OutQuint).OnComplete(() => {
+            _consumable2CooldownImg.DOFillAmount(0, Player.Instance.ConsumableCooldown.CooldownTime - 0.05f * Settings.AnimationSpeed).SetEase(Ease.Linear).OnComplete(() => {
+                consumableRect.DOShakeRotation(0.1f * Settings.AnimationSpeed, 10, 10, 90, false);
+                consumableRect.DOScale(Vector3.one * 1.05f, 0.1f * Settings.AnimationSpeed).SetEase(Ease.OutQuint).OnComplete(() => {
+                    consumableRect.DOScale(Vector3.one, 0.1f * Settings.AnimationSpeed).SetEase(Ease.OutQuint);
+                    consumableRect.localRotation = Quaternion.identity;
+                });
+            });
+        });
+    }
+
+
     public void ShowDashCooldown() {
         _dashCooldownRect.DOKill();
         _dashCooldownImg.DOKill();
@@ -59,6 +164,7 @@ public class HUDCanvas : MonoBehaviour, IUICanvasState
                 _dashCooldownRect.DOShakeRotation(0.1f * Settings.AnimationSpeed, 10, 10, 90, false);
                 _dashCooldownRect.DOScale(Vector3.one * 1.05f, 0.1f * Settings.AnimationSpeed).SetEase(Ease.OutQuint).OnComplete(() => {
                     _dashCooldownRect.DOScale(Vector3.one, 0.1f * Settings.AnimationSpeed).SetEase(Ease.OutQuint);
+                    _dashCooldownRect.localRotation = Quaternion.identity;
                 });
             });
         });
--- Assets/!/Scripts/UI/Inventory/InvTileEquipment.cs
+++ Assets/!/Scripts/UI/Inventory/InvTileEquipment.cs
@@ -55,18 +55,20 @@ public class InvTileEquipment : InvTile {
                 break; 
             
             case TileType.Consumeable1:
-                if (Player.Instance.ConsumableItemOne != null) {
+                Player.Instance.UpdateConsumablesEvent.AddListener(OnConsumablesUpdate);
+                if (Player.Instance.ConsumableItemOne.ItemData != null) {
                     InventoryItem inventoryItem = new();
-                    inventoryItem.ItemData = Player.Instance.ConsumableItemOne;
+                    inventoryItem.ItemData = Player.Instance.ConsumableItemOne.ItemData;
                     createItemUI(inventoryItem);
                     IsEmpty = false;
                 }
                 break;
             
             case TileType.Consumeable2:
-                if (Player.Instance.ConsumableItemTwo != null) {
+                Player.Instance.UpdateConsumablesEvent.AddListener(OnConsumablesUpdate);
+                if (Player.Instance.ConsumableItemTwo.ItemData != null) {
                     InventoryItem inventoryItem = new();
-                    inventoryItem.ItemData = Player.Instance.ConsumableItemTwo;
+                    inventoryItem.ItemData = Player.Instance.ConsumableItemTwo.ItemData;
                     createItemUI(inventoryItem);
                     IsEmpty = false;
                 }
@@ -77,6 +79,14 @@ public class InvTileEquipment : InvTile {
 
     private void OnTileSizeSetEvent() {
         _rectTransform.sizeDelta = new Vector2(InventoryPanel.TileSize, InventoryPanel.TileSize);
+    }
+
+    public void UpdateInvTile() {
+        if (_itemUI.InventoryItem.Amount <= 0) {
+            Destroy(_itemUI.gameObject);
+            _itemUI = null;
+            IsEmpty = true;
+        } 
     }
 
     private void OnItemUIClickEvent(ItemUI itemUI) {
@@ -98,9 +108,11 @@ public class InvTileEquipment : InvTile {
                     break;
                 case TileType.Consumeable1:
                     Player.Instance.ConsumableItemOne = null;
+                    Player.UICanvas.HUDCanvas.OnUpdateConsumables();
                     break;
                 case TileType.Consumeable2:
                     Player.Instance.ConsumableItemTwo = null;
+                    Player.UICanvas.HUDCanvas.OnUpdateConsumables();
                     break;
             }
 
@@ -119,7 +131,7 @@ public class InvTileEquipment : InvTile {
         if(!IsEmpty) return;
 
         
-        try {
+        
             if (_tileType == TileType.Armor) {
                 if (SelectedInventoryItem.ItemData is not ArmorItemData armorItemData) {
                     return;
@@ -143,26 +155,40 @@ public class InvTileEquipment : InvTile {
                     return;
                 }
 
-                Player.Instance.ConsumableItemOne = consumableItemData;
+                Player.Instance.ConsumableItemOne = SelectedInventoryItem;
+                Player.UICanvas.HUDCanvas.OnUpdateConsumables();
             } else if (_tileType == TileType.Consumeable2) {
                 if (SelectedInventoryItem.ItemData is not ConsumableItemData consumableItemData) {
                     return;
                 }
 
-                Player.Instance.ConsumableItemTwo = consumableItemData;
+                Player.Instance.ConsumableItemTwo = SelectedInventoryItem;
+                Player.UICanvas.HUDCanvas.OnUpdateConsumables();
             }
 
 
-        } catch (System.Exception e) {
-            Debug.Log(e);
-            return;
-        }
+        
 
         createItemUI(SelectedInventoryItem);
 
         EventBus.ItemPlacedEvent?.Invoke();
         IsEmpty = false;
         Player.UICanvas.SelectedItemUI.InventoryItem = null;
+    }
+
+    private void OnConsumablesUpdate() {
+        InventoryItem consumable = _tileType == TileType.Consumeable1 ? Player.Instance.ConsumableItemOne : Player.Instance.ConsumableItemTwo;
+
+        if (consumable.ItemData == null) {
+            if (_itemUI != null) {
+                Destroy(_itemUI.gameObject);
+                _itemUI = null;
+                IsEmpty = true;
+            }
+        }  else {
+            _itemUI.InventoryItem = consumable;
+            _itemUI.UpdateAmount();
+        }
     }
 
     private GameObject createItemUI(InventoryItem inventoryItem){
