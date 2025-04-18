38163d9c3a4b5a926a4f509c34044fbb11d94359
mikkk <108144274+JustMikkk@users.noreply.github.com>
2025-04-18 23:43:27 +0200

    feat: death animation

--- Assets/!/Scripts/HitFlashAnimator.cs
+++ Assets/!/Scripts/HitFlashAnimator.cs
@@ -4,20 +4,24 @@ public class HitFlashAnimator : MonoBehaviour
 {
     [SerializeField] private Material _hitFlashMaterial;
     [SerializeField] private float _flashDuration = 0.1f;
+    [SerializeField] private bool _isPlayer;
     private SkinnedMeshRenderer _meshRenderer;
     private Material _originalMaterial;
 
     private void Awake() {
+        if (_isPlayer) return;
         _meshRenderer = GetComponentInChildren<SkinnedMeshRenderer>();
         _originalMaterial = _meshRenderer.materials[0];
     }
 
     public void Flash() {
+        if (_isPlayer) return;
         _meshRenderer.materials = new Material[] { _hitFlashMaterial };
         Invoke(nameof(resetMaterial), _flashDuration);
     }
 
     private void resetMaterial() {
+        if (_isPlayer) return;
         _meshRenderer.materials = new Material[] {_originalMaterial};
     }
 }
