0164c77d7ff92300edde25512191318a6271e11a
Dero35 <antek.ciesielski.2006@gmail.com>
2025-04-26 17:10:43 +0200

    Mushroom pull VFX tweaks

--- Assets/!/Scripts/VFX/MushroomPull/RotateOverLifetime.cs
+++ Assets/!/Scripts/VFX/MushroomPull/RotateOverLifetime.cs
@@ -2,10 +2,9 @@ using UnityEngine;
 
 public class RotateOverLifetime : MonoBehaviour
 {
-    public float rotationSpeed = 45f; // Degrees per second
+    public float rotationSpeed = 360f;
     void Update()
     {
-        // Rotate the object around its Y-axis at a speed of 45 degrees per second
-        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+        transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
     }
 }
