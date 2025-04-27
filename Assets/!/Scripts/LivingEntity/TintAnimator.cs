f6d6058b1b97d9c4f9992af2205b5beb494afc49
mikkk <108144274+JustMikkk@users.noreply.github.com>
2025-04-27 02:20:13 +0200

    feat: multiple materials tint

--- Assets/!/Scripts/LivingEntity/TintAnimator.cs
+++ Assets/!/Scripts/LivingEntity/TintAnimator.cs
@@ -1,26 +1,40 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TintAnimator : MonoBehaviour
 {
-    [SerializeField] private SkinnedMeshRenderer _meshRenderer;
-    private Material _material;
+    [SerializeField] private List<SkinnedMeshRenderer> _meshRenderers;
+    private List<Material> _materials = new();
 
-    private void Awake() {
-        if (_meshRenderer == null) return;
-        _material = _meshRenderer.materials[0];
+    private void Start() {
+        if (_meshRenderers.Count == 0) return;
+        foreach (SkinnedMeshRenderer s in _meshRenderers) {
+            if (s == null) continue;
+            Debug.Log(s.materials[0]);
+            _materials.Add(s.materials[0]);
+        }
     }
 
     public void SetTint(Color color, float alpha, float duration) {
-        if (_meshRenderer == null) return;
-        _material.SetColor("_TintColor", color);
-        _material.SetFloat("_Alpha", alpha);
+        if (_meshRenderers.Count == 0) return;
+        Debug.Log(_materials.Count);
+
+        foreach (Material m in _materials) {
+            if (m == null) continue;
+            Debug.Log(m);
+            m.SetColor("_TintColor", color);
+            m.SetFloat("_Alpha", alpha);
+        }
+
         Invoke(nameof(ResetTint), duration);
     }
 
     public void ResetTint() {
-        if (_meshRenderer == null) return;
-        _material.SetColor("_TintColor", Color.white);
-        _material.SetFloat("_Alpha", 0f);
+        foreach (Material m in _materials) {
+            if (m == null) continue;
+            m.SetColor("_TintColor", Color.white);
+            m.SetFloat("_Alpha", 0f);
+        }
     }
     
 }
