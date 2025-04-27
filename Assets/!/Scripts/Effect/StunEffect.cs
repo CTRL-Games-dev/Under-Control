046dba6f2ee7c1cfabbe67ab8f86e877b75afacf
londek <48859717+londek@users.noreply.github.com>
2025-04-27 17:22:10 +0200

    chore: fix merge

--- Assets/!/Scripts/Effect/StunEffect.cs
+++ Assets/!/Scripts/Effect/StunEffect.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "SO_Eff_StunEffect", menuName = "Effect/StunEffect")]
 public class StunEffect : Effect {
-    public override void Apply(LivingEntity victim) {
+    public override void OnApply(LivingEntity victim) {
         victim.OnStunned.Invoke(Duration);
     }
 }
