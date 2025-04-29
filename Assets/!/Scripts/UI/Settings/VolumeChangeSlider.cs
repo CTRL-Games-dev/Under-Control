c047eca34d1a79b14d0ae01d48e12324d142a020
londek <48859717+londek@users.noreply.github.com>
2025-04-30 00:44:24 +0200

    fix: make volume logarithmic and percentage

--- Assets/!/Scripts/UI/Settings/VolumeChangeSlider.cs
+++ Assets/!/Scripts/UI/Settings/VolumeChangeSlider.cs
@@ -1,24 +1,32 @@
-using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 
-public class VolumeChangeSlider : MonoBehaviour
-{
+public class VolumeChangeSlider : MonoBehaviour {
     [SerializeField] private AudioMixerGroup _audioMixerGroup;
     [SerializeField] private Slider _slider;
     [SerializeField] private TextMeshProUGUI _valueText;
     [SerializeField] private string _volumeParameterName = "Volume";
+    [SerializeField, HideInInspector] private float _originalVolume = 0f;
 
     private void Start() {
-        _audioMixerGroup.audioMixer.SetFloat(_volumeParameterName, 0f);
+        _audioMixerGroup.audioMixer.GetFloat(_volumeParameterName, out _originalVolume);
+
         _slider.onValueChanged.AddListener(OnSliderValueChanged);
     }
 
     private void OnSliderValueChanged(float value) {
-        _audioMixerGroup.audioMixer.SetFloat(_volumeParameterName, value);
-        _valueText.text = ((int)(value)).ToString() + "db";
+        if (value < 0.001f) {
+            _audioMixerGroup.audioMixer.SetFloat(_volumeParameterName, -120);
+            _valueText.text = "0%";
+            return;
+        }
+
+        float gain = Mathf.Log10(value) * 20 + _originalVolume;
+
+        _audioMixerGroup.audioMixer.SetFloat(_volumeParameterName, gain);
+        _valueText.text = $"{(int)(value*100)}%";
 
     }
 }
