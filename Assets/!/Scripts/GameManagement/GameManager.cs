0b3799a00d470c1c4886e87640e8fcedee29c056
Poshaikk <olafik.22002277@gmail.com>
2025-05-05 13:17:13 +0200

    ice shard and camera

--- Assets/!/Scripts/GameManagement/GameManager.cs
+++ Assets/!/Scripts/GameManagement/GameManager.cs
@@ -178,7 +178,6 @@ public class GameManager : MonoBehaviour {
     public void OnLevelLoaded() {
         Player.UICanvas.ChangeUIMiddleState(UIMiddleState.Choose);
         LevelLoaded.Invoke();
-        Player.Instance.CameraDistance = 10;
     }
 
 
