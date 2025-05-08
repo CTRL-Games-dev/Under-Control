0c4b60af0ee3e1b97d56cc3963ff0c9395703598
londek <48859717+londek@users.noreply.github.com>
2025-05-09 00:38:57 +0200

    fix: inventory bound check logic

--- Assets/!/Scripts/Inventory/EntityInventory.cs
+++ Assets/!/Scripts/Inventory/EntityInventory.cs
@@ -10,7 +10,6 @@ public abstract class EntityInventory : MonoBehaviour {
     public abstract InventoryItem GetInventoryItem(Vector2Int position);
     public abstract List<InventoryItem> GetItems();
     public abstract bool IsWithinBounds(Vector2Int position);
-    public abstract bool FitsWithinBounds(Vector2Int position, Vector2Int size);
     public abstract bool CanBeAdded(ItemData itemData, int quantity, Vector2Int position, float powerScale = 1);
     public abstract bool RemoveInventoryItem(InventoryItem inventoryItem);
     public abstract bool RemoveItemAt(Vector2Int position);
