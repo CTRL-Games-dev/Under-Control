3b67a76f59f1ee6ad31d53ae585fbd63e58a10d4
Poshaikk <olafik.22002277@gmail.com>
2025-05-12 21:43:24 +0200

    pauldrons, hurtboxes

--- Assets/!/Scripts/Inventory/ItemDatas/ArmorItemData.cs
+++ Assets/!/Scripts/Inventory/ItemDatas/ArmorItemData.cs
@@ -3,14 +3,16 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "ArmorItemData", menuName = "Items/Armor")]
 public class ArmorItemData : ItemData {
-    [Serializable]
-    public struct DamageResistance {
-        public DamageType DamageType;
+    // [Serializable]
+    // public struct DamageResistance {
+    //     public DamageType DamageType;
 
-        [Range(0, 1)]
-        public float Resistance;
-    }
+    //     [Range(0, 1)]
+    //     public float Resistance;
+    // }
     
-    [SerializeField]
-    public DamageResistance[] DamageResistances;
+    // [SerializeField]
+    // public DamageResistance[] DamageResistances;
+    public int Armor;
+    public Material Material;
 }
