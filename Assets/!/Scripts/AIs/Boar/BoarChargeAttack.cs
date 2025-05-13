51889e111ae6280dc0155a907df469b5e36c2bdf
londek <48859717+londek@users.noreply.github.com>
2025-05-13 23:25:42 +0200

    fix: various changes

--- Assets/!/Scripts/AIs/Boar/BoarChargeAttack.cs
+++ Assets/!/Scripts/AIs/Boar/BoarChargeAttack.cs
@@ -1,22 +1,10 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Weapon))]
-public class BoarChargeAttack : MonoBehaviour {
+public class BoarChargeAttack : MonoBehaviour, IAttackAddon {
     public Effect StunEffect;
 
-    private List<LivingEntity> _alreadyHit = new List<LivingEntity>();
-
-    void Start() {
-        Weapon weapon = GetComponent<Weapon>();
-
-        weapon.OnHit.AddListener(OnHit);
-    }
-
-    private void OnHit(LivingEntity victim) {
-        if (_alreadyHit.Contains(victim)) return;
-        
-        _alreadyHit.Add(victim);
+    public void OnHit(LivingEntity victim) {
         victim.ApplyEffect(StunEffect);
         victim.OnStunned.Invoke(StunEffect.Duration);
     }
